23589e9db98ad479b4b166708148268167c1c067
Cam Millsap <cam.millsap12@gmail.com>
2021-11-17 10:59:22 -0500

    Manipulating the UI

--- SheepRescue-Starter/SheepRescue-Starter/Assets/RW/Scripts/Managers/GameStateManager.cs
+++ SheepRescue-Starter/SheepRescue-Starter/Assets/RW/Scripts/Managers/GameStateManager.cs
@@ -34,17 +34,20 @@ public class GameStateManager : MonoBehaviour
     public void SavedSheep()
     {
         sheepSaved++;
+        UIManager.Instance.UpdateSheepSaved();
     }
 
     private void GameOver()
     {
         sheepSpawner.canSpawn = false;
         sheepSpawner.DestroyAllSheep();
+        UIManager.Instance.ShowGameOverWindow();
     }
 
     public void DroppedSheep()
     {
         sheepDropped++;
+        UIManager.Instance.UpdateSheepDropped();
 
         if(sheepDropped == sheepDroppedBeforeGameOver)
         {
